12f618cac6c73a57a35426716fd070e3744a4395
kimnayoung <kny200208@naver.com>
2024-05-10 00:15:47 +0900

    .

--- chaebot/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py
-from django.urls import path
-from myapp.views import home
-
-urlpatterns = [
-    path("", home, name="home"),  # 루트 URL에 대한 처리
-    path("home/", home, name="home"),  # /home/에 대한 처리
-]
--- chaebot/static/js/home.js
+++
@@ -1,9 +0,0 @@
-function uploadFiles() {
-  const fileInput = document.getElementById("fileInput");
-  const files = fileInput.files;
-  if (files.length > 0) {
-    document.getElementById("convertBtn").disabled = false;
-  }
-}
-
-document.getElementById("fileInput").addEventListener("change", uploadFiles);
