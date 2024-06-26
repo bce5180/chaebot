54a6e2274f655eed4a2d52365d4e74ac15c653ce
nnaX000 <20211307@sungshin.ac.kr>
2024-06-26 16:44:56 +0900

    fix: 100%에 맞춰서 레이아웃 배율 변경

--- chaebot/static/js/script.js
+++ chaebot/static/js/script.js
@@ -119,3 +119,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.querySelector(".content").style.display = "block";
+  }, 2000); // 2초 후에 content 보이기
+});
