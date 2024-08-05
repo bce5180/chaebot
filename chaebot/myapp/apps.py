25f45ba786b4c26f54abe9d1612d4527240de2be
김나영 <kny200208@naver.com>
2024-08-06 01:40:05 +0900

    fix : 결과창 디자인 수정

--- chaebot/myapp/apps.py
+++ chaebot/myapp/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
 
-
-class MyappConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "myapp"
+class MyAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myapp'
 
     def ready(self):
-        import myapp.userSave
+        pass
+        # import myapp.userSave  # 주석 처리하여 임포트를 일시적으로 비활성화
